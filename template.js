8114e2060432a90686e00c91434985fdd08bd22a
Benedict <benedict_wallis@yahoo.co.uk>
2016-03-08 12:37:18 +0100

    Fixed template.js issues

--- template.js
+++ template.js
@@ -11,10 +11,10 @@
 exports.description = 'Create a basic Wordpress childtheme for Pressgang.';
 
 // template-specific notes displayed before question prompts
-// exports.notes = '';
+exports.notes = '';
 
 // template-specific notes displayed after question prompts
-//exports.after = '';
+exports.after = '';
 
 // any existing file or directory matching this wildcard will cause a warning
 exports.warnOn = '*';
@@ -22,17 +22,17 @@ exports.warnOn = '*';
 // the actual init template.
 exports.template = function(grunt, init, done) {
 
-    init.process({type: 'jquery'}, [
+    init.process({}, [
 
         // prompt
-        init.prompt('name', 'pressgang-child'),
-        init.prompt('homepage', 'https://github.com/benedict-w/pressgang'),
-        init.prompt('description', 'Wordpress child theme for pressgang.'),
-        init.prompt('version', '1.0.0'),
-        init.prompt('licenses', 'GPL-2.0'),
-        init.prompt('author_name', 'Benedict Wallis'),
-        init.prompt('author_email', 'ben@benedict-wallis.com'),
-        init.prompt('author_uri', 'http://www.benedict-wallis.com')
+        init.prompt('name', "pressgang-child"),
+        init.prompt('homepage', "https://github.com/benedict-w/pressgang-child"),
+        init.prompt('description', "Wordpress child theme for pressgang."),
+        init.prompt('version', "1.0.0"),
+        // init.prompt('licenses', "GPL-2.0"),
+        init.prompt('author_name', "Benedict Wallis"),
+        init.prompt('author_email', "ben@benedict-wallis.com"),
+        init.prompt('author_uri', "http://www.benedict-wallis.com")
 
     ], function(err, props) {
 
@@ -40,10 +40,12 @@ exports.template = function(grunt, init, done) {
         var files = init.filesToCopy(props);
 
         // add licenses
-        init.addLicenseFiles(files, props.licenses);
+        // init.addLicenseFiles(files, props.licenses);
+
+        console.log( files );
 
         // process and copy
-        init.copyAndProcess(files, props, {noProcess: ''});
+        init.copyAndProcess(files, props);
 
         // generate package.json file for npm and grunt
         init.writePackageJSON('package.json', props);
