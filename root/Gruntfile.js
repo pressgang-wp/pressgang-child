a500ade0ab0e09684789c5456b23d912a883215d
Benedict <ben@benedict-wallis.com>
2021-03-01 18:08:15 +0000

    Removed uglify-es

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -183,7 +183,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compress');
